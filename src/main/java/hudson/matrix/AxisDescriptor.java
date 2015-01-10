f10684402704bb0cda1b758f0e0ecf502d442098
Oliver Gondža <ogondza@gmail.com>
2015-01-10 18:49:12 +0100

    [FIXED JENKINS-23614] URL isn't escaped properly (leads to an 404 error)

--- src/main/java/hudson/matrix/AxisDescriptor.java
+++ src/main/java/hudson/matrix/AxisDescriptor.java
@@ -26,8 +26,10 @@ package hudson.matrix;
 import hudson.Util;
 import hudson.model.Descriptor;
 import hudson.model.Failure;
+import hudson.model.Messages;
 import jenkins.model.Jenkins;
 import hudson.util.FormValidation;
+
 import org.kohsuke.stapler.QueryParameter;
 
 /**
@@ -52,10 +54,47 @@ public abstract class AxisDescriptor extends Descriptor<Axis> {
 
     /**
      * Makes sure that the given name is good as a axis name.
+     *
+     * Aside from {@link Jenkins#checkGoodName()} this disallows ',' and
+     * '=' as special characters used in Combination presentation.
      */
     public FormValidation doCheckName(@QueryParameter String value) {
         if(Util.fixEmpty(value)==null)
             return FormValidation.ok();
+
+        if (value.contains(",")) return FormValidation.error(
+                Messages.Hudson_UnsafeChar(',')
+        );
+
+        if (value.contains("=")) return FormValidation.error(
+                Messages.Hudson_UnsafeChar('=')
+        );
+
+        try {
+            Jenkins.checkGoodName(value);
+            return FormValidation.ok();
+        } catch (Failure e) {
+            return FormValidation.error(e.getMessage());
+        }
+    }
+
+    /**
+     * Makes sure that the given name is good as a axis name.
+     *
+     * Aside from {@link Jenkins#checkGoodName()} this disallows ',' as
+     * special character used in Combination presentation. Note it is not
+     * necessary to disallow '=' in value as everything after the first
+     * occurrence is considered to be a value.
+     *
+     * Subclasses are expected to expose own <tt>doCheck</tt> method possibly delegating to this one.
+     */
+    public FormValidation checkValue(@QueryParameter String value) {
+        if(Util.fixEmpty(value)==null)
+            return FormValidation.ok();
+
+        if (value.contains(",")) return FormValidation.error(
+                Messages.Hudson_UnsafeChar(',')
+        );
 
         try {
             Jenkins.checkGoodName(value);
---
+++ src/test/java/hudson/matrix/AxisDescriptorTest.java
@@ -0,0 +1,70 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015 Red Hat, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package hudson.matrix;
+
+import static org.junit.Assert.*;
+import hudson.util.FormValidation;
+
+import org.junit.Test;
+
+public class AxisDescriptorTest {
+
+    TextAxis.DescriptorImpl descriptor = new TextAxis.DescriptorImpl();
+
+    @Test
+    public void combinationNameSpecialChars() {
+        assertEquals(
+                FormValidation.Kind.ERROR,
+                descriptor.doCheckName("a=b").kind
+        );
+
+        assertEquals(
+                FormValidation.Kind.ERROR,
+                descriptor.doCheckName("a,b").kind
+        );
+
+        assertEquals(
+                FormValidation.Kind.ERROR,
+                descriptor.doCheckName("a/b").kind
+        );
+    }
+
+    @Test
+    public void combinationValueSpecialChars() {
+        assertEquals(
+                FormValidation.Kind.OK,
+                descriptor.checkValue("a=b").kind
+        );
+
+        assertEquals(
+                FormValidation.Kind.ERROR,
+                descriptor.checkValue("a,b").kind
+        );
+
+        assertEquals(
+                FormValidation.Kind.ERROR,
+                descriptor.checkValue("a/b").kind
+        );
+    }
+}
