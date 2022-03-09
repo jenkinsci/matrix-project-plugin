616bc571d3f2a6cdbeeb5d57faec23b44414bb5b
Tim Jacomb <timjacomb1+github@gmail.com>
2022-03-09 09:02:03 +0000

    Bump parent, enable dependabot

--- src/test/java/hudson/matrix/CombinationFilterUsingBuildParamsTest.java
+++ src/test/java/hudson/matrix/CombinationFilterUsingBuildParamsTest.java
@@ -23,8 +23,8 @@
  */
 package hudson.matrix;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
